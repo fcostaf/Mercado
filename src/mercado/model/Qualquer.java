d69ac5bf8946716c7454d0bd668794adce558e63
fcostaf <fcostaf@yahoo.com.br>
2023-08-22 10:28:44 -0300

    Desenvolvimento da classe Qualquer na branch classesTeste

--- src/mercado/model/Qualquer.java
+++ src/mercado/model/Qualquer.java
@@ -10,5 +10,38 @@ package mercado.model;
  * @author 771000343
  */
 public class Qualquer {
+    private int id;
+    private String nome;
+    private String cpf;
+
+    public Qualquer(int id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
     
 }
